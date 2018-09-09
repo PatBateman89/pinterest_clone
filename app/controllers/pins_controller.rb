abdc43e9c28621be914ffa7e63bcd233299a4560
PatBateman89 <feric89@gmail.com>
2018-09-09 19:55:34 +0900

    CRUD added, and gems

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,51 @@
+class PinsController < ApplicationController
+  def index
+    @pins = Pin.all.order('created_at DESC')
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def show
+    @pin = Pin.find(params[:id])
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created your Pin!"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @pin = Pin.find(params[:id])
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was Successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin = Pin.find(params[:id])
+    @pin.destroy
+
+    redirect_to pins_path, notice: "Pin was Successfully Deleted!"
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+
+end
